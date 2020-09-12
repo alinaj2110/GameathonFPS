f0d1c6c6f70ab959a5323b1c36c4265219a0f1ef
alinaj2110 <anjalipraveen2110@gmail.com>
2020-09-12 11:57:31 +0530

    randomspawnmovement applied

---
+++ Assets/Scripts/AlienRandomSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienRandomSpawner : MonoBehaviour
+{
+    public GameObject core;
+    public GameObject[] alienPrefabs;
+    public float spawnX = 20.0f;
+    public float spawnZ = 30.0f;
+    public float timedelay = 2.0f;
+    public float repRate=2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for(int i=0; i< repRate.Length; i++)
+        transform.position = core.transform.position;
+        InvokeRepeating("AlienSpawn",timedelay, repRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //spawn function
+    void AlienSpawn()
+    {
+        int index = Random.Range(0, alienPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnX, spawnX), 1, Random.Range(-spawnZ,spawnZ));
+        Instantiate(alienPrefabs[index], spawnPos, alienPrefabs[index].transform.rotation);
+    }
+}
