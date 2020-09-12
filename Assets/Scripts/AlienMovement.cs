f3bd549be1994ce0fea88a3dd4be189d0e42363c
alinaj2110 <anjalipraveen2110@gmail.com>
2020-09-12 10:15:01 +0530

    alienmovement whenever player is within the range

---
+++ Assets/Scripts/AlienMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienMovement : MonoBehaviour
+{
+    public float speed=5.0f;
+    public float alienAttackRange=20.0f;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) < alienAttackRange)
+        {
+            //alien moves when player comes near it
+            //Ray ray = new Ray(transform.position, player.transform.position);
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        }
+    }
+}
